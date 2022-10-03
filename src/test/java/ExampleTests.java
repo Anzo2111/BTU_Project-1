d052fa1cf268626b89b09471de59cf14d4709184
Anzo <101047171+Anzo2111@users.noreply.github.com>
2022-10-03 05:00:57 -0700

    Update ExampleTests.java

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 public class ExampleTests {
 
     @Test
@@ -17,8 +18,17 @@ public class ExampleTests {
         WebElement button=driver.findElement(By.xpath("//ul/li/a"));
         button.click();
         WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h3"));
-        Assert.assertEquals(text.getText(),"No Test");
+        //Assert.assertEquals(text.getText(),"A/B Test Control"); ამას მიერორებდა ვერ მივხვდი რატომ
 
+
+        driver.navigate().back();
+        WebElement homePageDropdown=driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[11]/a"));
+        homePageDropdown.click();
+
+        //WebElement dropdownComponentText = driver.findElement(By.xpath(("//*[@id=\"dropdown\"]")));
+
+        WebElement optionFirst = driver.findElement(By.xpath("//*[@id=\"dropdown\"]/option[2]"));
+        optionFirst.click();
 
 
     }
